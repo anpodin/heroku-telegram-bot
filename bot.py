bd7be2f6252895df7d289628d506890bd16c373c
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 15:13:58 +0300

    Update bot.py
    Adding the variables and behaviour

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import telebot
 
 # Example of your code beginning
 #           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
+token = os.environ['607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls']
+some_api_token = os.environ['7735026c-53c3-4cf4-bb08-907271fea7f4']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
@@ -17,4 +17,15 @@ r = redis.from_url(os.environ.get("REDIS_URL"))
 #       Your bot code below
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
-#              ...
+#            
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+    
+    elif message.text == "How are you?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+    
+    else:
+        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")  ...
